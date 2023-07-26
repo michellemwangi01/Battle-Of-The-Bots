3231bb1501fd68d16b0e112bca9bf63be4e7b0df
michellemwangi01 <michellemwangim@gmail.com>
2023-07-27 00:45:17 +0300

    added TOAST noticiations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
 import "../src/Styles/CardStyles.css";
 import BotCollection from "./Components/BotCollection";
-import BotArmy from "./Components/BotArmy";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const notify = () => toast("Wow so easy!");
+
   return (
     <div className="App">
+      {/* <button onClick={notify}>Toastttt</button> */}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <BotCollection />
     </div>
   );
--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
 import BotArmy from "./BotArmy";
 import BotCard from "./BotCard";
+import { ToastContainer, toast } from "react-toastify";
 
 const BotCollection = () => {
   const [botData, setBotData] = useState([]);
   const [addToArmyID, setAddToArmyID] = useState();
   const [botArmy, setBotArmy] = useState([]);
+
+  const noDuplicateBotsWarning = (name) =>
+    toast(
+      `Bot ${name.toUpperCase()} is already in the army and cannot be added again!`,
+      {
+        type: "warning",
+      }
+    );
+  const botAddedToArmySuccessfully = (name) =>
+    toast(`Bot ${name.toUpperCase()} has been sucessfully added to Army!`, {
+      type: "success",
+    });
 
   function addToArmy(botId) {
     const botArmyItem = botData.find((bot) => bot.id == botId);
@@ -13,6 +26,9 @@ const BotCollection = () => {
     if (!matchingBot) {
       let updatedArmy = [...botArmy, botArmyItem];
       setBotArmy(updatedArmy);
+      botAddedToArmySuccessfully(botArmyItem.name);
+    } else {
+      noDuplicateBotsWarning(botArmyItem.name);
     }
   }
   console.log(botArmy);
